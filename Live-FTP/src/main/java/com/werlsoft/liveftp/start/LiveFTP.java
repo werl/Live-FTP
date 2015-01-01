5f304332fed082b5af849272d7f7e799eaa4bd5e
Peter L <peter@werl.me>
2015-01-01 14:08:01 -0700

    first working code

--- Live-FTP/src/main/java/com/werlsoft/liveftp/start/LiveFTP.java
+++ Live-FTP/src/main/java/com/werlsoft/liveftp/start/LiveFTP.java
@@ -1,8 +1,29 @@
 package com.werlsoft.liveftp.start;
 
-public class LiveFTP {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		
+import it.sauronsoftware.ftp4j.FTPAbortedException;
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferException;
+import it.sauronsoftware.ftp4j.FTPException;
+import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
+import it.sauronsoftware.ftp4j.FTPListParseException;
+
+public class LiveFTP {
+	
+	public static String hostName = "ftp.werl.me";
+	private static String password = "";
+	public static String username = "";
+	public static int ftpPort = 21;	
+
+	public static void main(String[] args) throws IllegalStateException, IOException, FTPIllegalReplyException, FTPException, FTPDataTransferException, FTPAbortedException, FTPListParseException {
+		FTPClient client = new FTPClient();
+		client.connect(hostName);
+		client.login(username, password);
+		String[] files = client.listNames();
+		for (String s: files){
+			System.out.println(s);
+		}
+		client.disconnect(true);
 	}
 }
