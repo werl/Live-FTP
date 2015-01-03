c2e3458de3f0505fe579ef8267d47eb5cc69b2fe
Peter L <peter@werl.me>
2015-01-02 21:01:36 -0700

    fixed args req bug

--- Live-FTP/src/main/java/com/werlsoft/liveftp/start/LiveFTP.java
+++ Live-FTP/src/main/java/com/werlsoft/liveftp/start/LiveFTP.java
@@ -7,33 +7,29 @@ import com.werlsoft.liveftp.gui.panel.BasePanel;
 import com.werlsoft.liveftp.gui.panel.MainPanel;
 
 public class LiveFTP {
-	
+
 	public static String hostName = "";
 	public static String userName = "";
 	private static String password = "";
-	public static int ftpPort = 21;	
-	
+	public static int ftpPort = 21;
+
 	private static MainWindow frame;
 
 	public static void main(String[] args) {
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						frame = new MainWindow(new MainPanel());
-						frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new MainWindow(new MainPanel());
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-		
-		hostName = args[0];
-		userName = args[1];
-		password = args[2];
+			}
+		});
 	}
-	
-	public static boolean setPanel (BasePanel panel) {
-		if(panel.getPanelID().matches(frame.getCurrentPanelID()))
+
+	public static boolean setPanel(BasePanel panel) {
+		if (panel.getPanelID().matches(frame.getCurrentPanelID()))
 			return false;
 		frame = new MainWindow(panel);
 		return true;
